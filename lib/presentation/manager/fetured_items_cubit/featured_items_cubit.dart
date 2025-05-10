de6531bdd0ecf6ecd9f30e03dfb92101d4aeabad
salama <imammuhammed797@gmail.com>
2025-05-10 15:50:38 +0300

    create featured items method

--- lib/presentation/manager/fetured_items_cubit/featured_items_cubit.dart
+++ lib/presentation/manager/fetured_items_cubit/featured_items_cubit.dart
@@ -1,9 +1,20 @@
 import 'package:bloc/bloc.dart';
+import 'package:furniswap/core/errors/failures.dart';
 import 'package:furniswap/data/models/furni_model.dart';
+import 'package:furniswap/data/repository/home_repo.dart';
 import 'package:meta/meta.dart';
 
 part 'featured_items_state.dart';
 
 class FeaturedItemsCubit extends Cubit<FeaturedItemsState> {
-  FeaturedItemsCubit() : super(FeaturedItemsInitial());
+  FeaturedItemsCubit(this.homeRepo) : super(FeaturedItemsInitial());
+  final HomeRepo homeRepo;
+  Future<void> fetchFeaturedItems() async {
+    var result = await homeRepo.FetchAllProducts();
+    result.fold((Failure) {
+      emit(FeaturedItemsFailure("Error"));
+    }, (furni) {
+      emit(FeaturedItemsSuccess(furni));
+    });
+  }
 }
